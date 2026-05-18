edf6051cefb1ea94caab609b1da13a0caae52a16
Nora Dwelly <dwelly.412@gmail.com>
2026-05-18 16:52:10 +0100

    feat: Add post closing

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,34 @@ let postContent = null;
 
 function onPostThumbnailClicked()
 {
-
+    // open the post - hide the pageContent article and show postContent
+    pageContent.classList.add("hidden");
+    postContent.classList.remove("hidden");
 }
 
-function onPostClosed()
+function closePost()
 {
-
+    postContent.classList.add("hidden");
+    pageContent.classList.remove("hidden");
 }
 
 function main()
 {
-    // get references to the page articles (post & main content)
-    
+    // initialise references
+    pageContent = document.querySelector(".content");
+    postContent = document.querySelector(".post");
+
+    let postAnchors = document.querySelectorAll(".post-thumb");
+
+    postAnchors.forEach(element => {
+        element.addEventListener("click", onPostThumbnailClicked);
+    });
+
+    document.querySelector("#close-post")
+    .addEventListener("click", closePost)
+
+    console.log("page loaded");
 }
+
+
+window.addEventListener("load", main);
