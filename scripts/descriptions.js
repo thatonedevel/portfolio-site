37a46743ee8cbfc66c26dbf03063dad2c6a03228
Nora Dwelly <dwelly.412@gmail.com>
2026-05-19 15:53:06 +0100

    feat: Add basic description for Blackout

--- scripts/descriptions.js
+++ scripts/descriptions.js
@@ -1,7 +1,10 @@
-export { DEFENDER };
+export { DEFENDER, BLACKOUT };
 
 
 // project descriptions
 const DEFENDER = `Moonbase Defender is a browser based game made using JS and the Phaser Engine. 
 It's a simple tower-defense game that runs in the browser, primarily influenced by the Bloons and PvZ series. As the player, you have to defend your base from waves of enemies by placing different objects around your base, each of which has an associated cost - to generatew these you need to make sure to place 
 `
+const BLACKOUT = `Blackout is an interactive choose-your-adventure story I made as part of a team of 4. This was built using a tool called Twine, and can be played in the browser.
+We made this game to study & apply narrative structures, such as the Hero's Journey, within a game context, whilst still providing some choice to the player. As part of this, each of us took up a different narrative branch to design - I primarily worked on the power plant section and office break-in section.
+Due to the nature of Twine, there wasn't really much technical involvment, as this was done moreso to study narrative design - at most it was some macros to define variables so we could determine which ending the player should end up at.`;
