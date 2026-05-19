89cbfb52758153b1e0ff6f98763c517bd87c2d19
Nora Dwelly <dwelly.412@gmail.com>
2026-05-19 15:21:08 +0100

    feat: Start adding posts

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,87 @@
+import { DEFENDER } from "./descriptions.js";
+
 let pageContent = null;
 let postContent = null;
 
+let posts = { // use this to store post references. it is shared state (so not great practice) but is fairly minor
+    moonbaseDefender: null,
+    blackout: null,
+    genericRPG: null,
+    euclideanDreams: null,
+    caverns: null
+}
+
 class Post {
     title;
-    imageSource;
+    image;
     textContent;
-    videoEmbed;
+    videoID;
 
-    constructor(title)
+    constructor(title, imageUrl, id)
     {
         this.title = title;
+        this.image = imageUrl;
+        this.videoID = id;
+    }
+
+    setPostContent(postRootElement)
+    {
+        // set the content of the post body on the page
+        let img = document.querySelector("#header-image");
+        let body = document.querySelector("#body-text");
+        let postTitle = document.querySelector("#post-title");
+
+        postTitle.textContent = this.title;
+        img.setAttribute("src", this.image);
+        body.textContent = this.textContent;
     }
 }
 
-function onPostThumbnailClicked()
-{
-    // open the post - hide the pageContent article and show postContent
-    pageContent.classList.add("hidden");
-    postContent.classList.remove("hidden");
-}
+class ImageGallery {
+    #images = [];
+    #element;
+    #index = 0;
 
-function closePost()
-{
-    postContent.classList.add("hidden");
-    pageContent.classList.remove("hidden");
+    constructor(...sources)
+    {
+        this.#element = document.createElement("img");
+        sources.forEach(element => {
+            this.images.push(element);
+        });
+    }
+
+    attach(parent)
+    {
+        parent.appendChild(this.#element);
+    }
+
+    addImage(source)
+    {
+        this.#images.push(source);
+    }
+
+    removeImage(index)
+    {
+        this.#images.splice(index, 1);
+    }
+
+    clear()
+    {
+        if (this.#images.length !== 0)
+            this.#images.splice(0);
+    }
+
+    next()
+    {
+        this.index = this.index == this.#images.length - 1? 0: this.index + 1;
+        this.#element.setAttribute("src", this.#images[index]);
+    }
+
+    previous()
+    {
+        this.index = this.index == 0? this.#images.length : this.index - 1;
+        this.#element.setAttribute("src", this.#images[index]);
+    }
 }
 
 function createPosts()
@@ -31,6 +89,28 @@ function createPosts()
     // make the "posts" in here
     // ideally we wouldn't hardcode them in the script but i'm not sure if
     // we could use a http request on a gh pages site
+
+    posts.moonbaseDefender = new Post("Moonbase Defender", 
+        "images/thumbmails/moonbase_defender_thumbnail.png",
+    "t0ecie6R_PM");
+
+    moonbaseDefender.textContent = DEFENDER;
+}
+
+function onPostThumbnailClicked()
+{
+    // open the post - hide the pageContent article and show postContent
+    pageContent.classList.add("hidden");
+    postContent.classList.remove("hidden");
+
+    // lookup the post from the posts object
+    console.log(this.target.id);
+}
+
+function closePost()
+{
+    postContent.classList.add("hidden");
+    pageContent.classList.remove("hidden");
 }
 
 function main()
