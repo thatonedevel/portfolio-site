c33af0c56288a9212e31c544bbe87135ffa82be5
Nora Dwelly <dwelly.412@gmail.com>
2026-05-19 14:50:52 +0100

    feat: Add script for storing post descriptions

---
+++ scripts/descriptions.js
@@ -0,0 +1,7 @@
+export { DEFENDER };
+
+
+// project descriptions
+const DEFENDER = `Moonbase Defender is a browser based game made using JS and the Phaser Engine. 
+It's a simple tower-defense game that runs in the browser, primarily influenced by the Bloons and PvZ series. As the player, you have to defend your base from waves of enemies by placing different objects around your base, each of which has an associated cost - to generatew these you need to make sure to place 
+`
