c1d06e35bc6d87aed78511ed26613acff9139b01
Nora Dwelly <dwelly.412@gmail.com>
2026-05-20 15:41:41 +0100

    feat: Added video-by-id support

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,13 @@ import { BLACKOUT, DEFENDER, GENERICRPG } from "./descriptions.js";
 
 let pageContent = null;
 let postContent = null;
+let vplayer = null;
+let videoContainer = null;
 
 let dummyPost = null; // placeholder post for error handling
+
+const EMBED_URL = "https://www.yout-ube.com/embed/";
+const EMBED_SETTINGS = "accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen";
 
 let posts = { // use this to store post references. it is shared state (so not great practice) but is fairly minor
     moonbaseDefender: null,
@@ -19,7 +24,7 @@ class Post {
     textContent;
     videoID;
 
-    constructor(title, imageUrl, id)
+    constructor(title, imageUrl, id=null)
     {
         this.title = title;
         this.image = imageUrl;
@@ -36,6 +41,15 @@ class Post {
         postTitle.textContent = this.title;
         img.setAttribute("src", this.image);
         body.textContent = this.textContent;
+
+        // check if we have an associated video
+        if (this.videoID !== null)
+        {
+            console.log(this.videoID);
+            vplayer.setAttribute("allow", EMBED_SETTINGS);
+            vplayer.setAttribute("src", EMBED_URL + this.videoID + "?rel=&autoplay=0");
+            videoContainer.classList.remove("hidden");
+        }
     }
 }
 
@@ -143,6 +157,7 @@ function closePost()
 {
     postContent.classList.add("hidden");
     pageContent.classList.remove("hidden");
+    videoContainer.classList.add("hidden");
 }
 
 function main()
@@ -162,6 +177,9 @@ function main()
 
     // create all the posts
     createPosts();
+
+    vplayer = document.querySelector("#player");
+    videoContainer = document.querySelector("#player-container");
 
     console.log("page loaded");
 }
