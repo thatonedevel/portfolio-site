72da043000c0df30f402fdd388afc959f0392ee0
Nora Dwelly <dwelly.412@gmail.com>
2026-05-25 12:59:50 +0100

    feat: Add means to insert gallery into a page

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,23 +1,64 @@
 export class ImageGallery {
     #images = [];
-    #element;
     #index = 0;
 
+    // element references
+    #galleryContainer;
+    #prevButton;
+    #nextButton;
+    #display;
 
-
-    constructor(...sources)
+    constructor(parent, ...sources)
     {
-        this.#element = document.createElement("img");
+        // creates the image gallery and sources
+
         sources.forEach(element => {
             this.images.push(element);
         });
 
         // create the tag structure for the gallery
-    }
+        this.#galleryContainer = Object.assign(document.createElement("div"), {
+            class: "gallery-container"
+        });
 
-    attach(parent)
-    {
-        parent.appendChild(this.#element);
+        this.#prevButton = Object.assign(document.createElement("button"), {
+            class: "gallery-button"
+        });
+
+        this.#nextButton = Object.assign(document.createElement("button"), {
+            class: "gallery-button"
+        });
+
+        this.#display = Object.assign(document.createElement("img"), {
+            class: "gallery-image",
+            src: this.#images[0]
+        });
+
+        // assign button callbacks
+        this.#nextButton.addEventListener("click", this.next);
+        this.#prevButton.addEventListener("click", this.previous);
+
+        // add the elements to the container & then the parent element
+        this.#galleryContainer.appendChild(this.#prevButton);
+        this.#galleryContainer.appendChild(this.#display);
+        this.#galleryContainer.appendChild(this.#nextButton);
+
+
+        // add icons to the buttons
+        this.#prevButton.appendChild(
+            Object.assign(document.createElement("i"), {
+                    class: "fa-solid fa-angle-left"
+                })
+        );
+
+        this.#nextButton.appendChild(
+            Object.assign(document.createElement("i"), {
+                class: "fa-solid fa-angle-right"
+            })
+        );
+
+        // add gallery to the page
+        parent.appendChild(this.#galleryContainer);
     }
 
     addImage(source)
@@ -41,19 +82,18 @@ export class ImageGallery {
         this.clear();
         newSources.forEach((source) => {
             this.#images.push(source);
-            
         });
     }
 
     next()
     {
         this.index = this.index == this.#images.length - 1? 0: this.index + 1;
-        this.#element.setAttribute("src", this.#images[index]);
+        this.#display.setAttribute("src", this.#images[index]);
     }
 
     previous()
     {
         this.index = this.index == 0? this.#images.length : this.index - 1;
-        this.#element.setAttribute("src", this.#images[index]);
+        this.#display.setAttribute("src", this.#images[index]);
     }
 }
